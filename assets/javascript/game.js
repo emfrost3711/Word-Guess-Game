ce1b7c9c9f1cf87d9c77a2d98325ab164f59c02c
Emily Frost <emfrost@gmail.com>
2019-05-01 17:49:46 -0600

    defining variables and effectively linked all the style and src jS sheets

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,12 +4,42 @@
     var winsText = document.getElementById("wins");
     var currentWord = document.getElementById("word")
     var lettersGuessed = document.getElementById("userGuess")
-    var guessesRemaining = 0; 
+    var badassLadies = [
+        "jane austen", 
+        "cleopatra",
+        "beyonce",
+        "michelle obama",
+        "oprah",
+        "frida kahlo"];
+    var word = badassLadies[Math.floor(Math.random() * badassLadies.length)];
+    var answerArray = [];
+    for (var i = 0; i < badassLadies.length; i++) {
+        answerArray[i] = "_";
+    }
+    var remainingLetters = badassLadies.length;
 
-    //Maybe do badass ladies in history? and their name & a quote pops up? 
+    //Maybe do badass ladies in history? and their name & a quote pops up
     //need variables for "Press any key to get started!" (then that should disappear after the game starts)
     //var: wins; var: current word; var: number of guesses remaining; var: letters already guessed:
-    //images to change via an if
+    //images to change when they get the answer right
+
+    //Pick a badass lady
+    //While the word has not been guessed {
+    //     show the player their current Progress 
+    //     get a guess from the player 
+
+    //     if the player wants to quite the game {
+    //         quit the game
+    //     }
+    //     else if the guess is not a single letter {
+    //         tell the player to pick a single letter
+    //     }
+    //     else {
+    //         if the guess is in the word {
+    //             update hte player's progress with the guess
+    //         }
+    //     }
+    // }
 
     //if user guesses same letter >1 no change to number of guesses remaining
 
