d591a56b4a81de03306ed913aa6b38bfc4390449
Emily Frost <emfrost@gmail.com>
2019-05-03 19:56:35 -0600

    added loop, getElementbyId from HTML, more pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,12 @@
-    
+      //Maybe do badass ladies in history? and their name & a quote pops up
+       //var: wins; var: current word; var: number of guesses remaining; var: letters already guessed:
     var wins = 0;
+    var losses = 0; 
     var directionsText = document.getElementById("directions");
     var winsText = document.getElementById("wins");
     var currentWord = document.getElementById("word");
     var lettersGuessed = document.getElementById("userGuess");
+    var losses = document.getElementById("losses"); 
     var badassLadies = [
         "jane austen", 
         "cleopatra",
@@ -13,17 +16,33 @@
         "frida kahlo"];
     var word = badassLadies[Math.floor(Math.random() * badassLadies.length)];
     var answerArray = [];
-    for (var i = 0; i < badassLadies.length; i++) {
-        answerArray[i] = "_";
+    for (var i = 0; i < word.length; i++) {
+        //if there is a space it puts a space in the answer array; if it's a letter it puts a _ in
+      if (word[i]===" ") {
+        answerArray.push(" ");
+      } 
+      else {answerArray.push("_");}
     }
-    var remainingLetters = badassLadies.length;
-
-    //Maybe do badass ladies in history? and their name & a quote pops up
+      //puts the _ _ _ on the page in the answer array 
+    currentWord.textContent = answerArray.join(' ');
+        console.log(answerArray.length)
+    var remainingGuesses = 10; 
+    console.log(word)
     //need variables for "Press any key to get started!" (then that should disappear after the game starts)
-    //var: wins; var: current word; var: number of guesses remaining; var: letters already guessed:
+
+
+    //game loop here
+    // while (remainingLetters > 0) {
+        //game code goes here
+        //show the player their progress
+        //take input guess from the player
+        //update answerArray and remainingLetters for every correct guess
+    // }
+
+
     //images to change when they get the answer right
 
-    //Pick a badass lady
+    //Computer picks a badass lady
     //While the word has not been guessed {
     //     show the player their current Progress 
     //     get a guess from the player 
@@ -36,7 +55,7 @@
     //     }
     //     else {
     //         if the guess is in the word {
-    //             update hte player's progress with the guess
+    //             update the player's progress with the guess
     //         }
     //     }
     // }
@@ -50,11 +69,28 @@
     //This is our first badass lady: Jane Austen. 
     document.onkeyup = function(event) {
         var userGuess = event.key.toLowerCase();
-    
-        if (userGuess === "j") //insert "j" into " _ _ _ _  _ _ _ _ _ _ "
-        {
+        console.log(userGuess)
+        //go through word; if userguess is a letter in the word, place it in the appropriate slot; 
+        //check to see if array has any underscores
+        //if there are no underscores, they win! increase wine count
+        //if the guesses remaining is above 0 remove 1 from remaining guesses and log the guess on the page;
+        // if guesses = 0, player loses
+        // when player loses, alert "You lose"; increase loss count; then reset it. 
+            //clear out lettersholldingthing; choose a new string from the badassLadies array; reset answers array; reset guesses to 10;
 
-        } else {
-            document.write () //add the letter to letters guessed AND move the guesses remaining number down 1
-        }
+
+
+        // if (userGuess === "j") //insert "j" into " _ _ _ _  _ _ _ _ _ _ "
+        // {
+            
+        // } else {
+        //     document.write () //add the letter to letters guessed AND move the guesses remaining number down 1
+        // }
     }
+
+
+
+
+    //make a new function that incluees setting guesses, letters guessed values, all the base values, puts the dashes on the pages, sets number og tuesses pickes the word sets the letters they've guessed to a value
+    //make a reset function that you can call repeatedly;
+    
